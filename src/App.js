a1d97aac386601aa9b5265584215e00206b598b2
fran <francisco.chiarino@gmail.com>
2020-08-27 12:17:52 +0200

    NavigationBar is working as expected

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 
 import React from 'react';
 import NavigationBar from './NavigationBar';
+import { HashRouter, Switch, Route } from 'react-router-dom'
+import People from './People';
+import Images from './Images';
+import Home from './Home';
+import './main.scss';
 
 function App() {
   return (
-    <div className="App">
-      <NavigationBar />
-    </div>
+    <>
+      <HashRouter>
+        <NavigationBar />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/people" component={People} />
+          <Route exact path="/images" component={Images} />
+        </Switch>
+      </HashRouter>
+    </>
   );
 }
 
